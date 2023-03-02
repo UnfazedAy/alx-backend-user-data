a2f500adb6e221c151eca8fe78402a33abdbfebc
UnfazedAy <ayomidesoniyi@gmail.com>
2023-03-02 16:55:30 +0100

    Done with task 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,9 @@ import logging
 import re
 
 
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
 # Start of task 0
 def filter_datum(
         fields: List[str], redaction: str, message: str, separator: str
@@ -20,6 +23,7 @@ def filter_datum(
     return message
 
 
+# start of task 1
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
         """
@@ -38,3 +42,17 @@ class RedactingFormatter(logging.Formatter):
         return filter_datum(
             self.fields, self.REDACTION, message, self.SEPARATOR
         )
+
+
+# Start of task 2
+def get_logger() -> logging.Logger:
+    """creates a logger"""
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    stream_handler = logging.StreamHandler()
+    formatter = RedactingFormatter(PII_FIELDS)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    return logger
+    
