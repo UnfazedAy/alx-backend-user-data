bc87d490e6ff81f51a1d5dd8374bf94e6731d92e
UnfazedAy <ayomidesoniyi@gmail.com>
2023-03-02 23:55:59 +0100

    Love this task

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,8 +7,8 @@ def hash_password(password: str) -> bytes:
     """Uses hashpw to encrypt password"""
     bytes = password.encode('utf-8')
     salt = bcrypt.gensalt()
-    hash = bcrypt.hashpw(bytes, salt)
-    return hash
+    hash_pw = bcrypt.hashpw(bytes, salt)
+    return hash_pw
 
     """In 1 line. I prefer the first though"""
     # return bcrypt.hashpw(b'password', bcrypt.gensalt())
