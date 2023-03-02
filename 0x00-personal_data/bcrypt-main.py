a52b4511b19bd7566debcb4cc0c7e788708a5443
UnfazedAy <ayomidesoniyi@gmail.com>
2023-03-03 00:16:48 +0100

    Done with the final task

--- 0x00-personal_data/bcrypt-main.py
+++ 0x00-personal_data/bcrypt-main.py
@@ -3,8 +3,17 @@
 Main file
 """
 
+# hash_password = __import__('encrypt_password').hash_password
+
+# password = "MyAmazingPassw0rd"
+# print(hash_password(password))
+# print(hash_password(password))
+
+# Task 6
 hash_password = __import__('encrypt_password').hash_password
+is_valid = __import__('encrypt_password').is_valid
 
 password = "MyAmazingPassw0rd"
-print(hash_password(password))
-print(hash_password(password))
+encrypted_password = hash_password(password)
+print(encrypted_password)
+print(is_valid(encrypted_password, password))
--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -4,5 +4,20 @@ import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """Uses hashpw to encrypt password"""
-    return bcrypt.hashpw(b'password', bcrypt.gensalt())
+    """
+    Uses hashpw to encrypt password
+    """
+    bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hash_pw = bcrypt.hashpw(bytes, salt)
+    return hash
+
+    """In 1 line. I prefer the first though"""
+    # return bcrypt.hashpw(b'password', bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Checks if a password is valid and matches the hashed pwd
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
