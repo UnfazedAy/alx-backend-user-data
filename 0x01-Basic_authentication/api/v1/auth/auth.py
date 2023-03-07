5d4bb65662e4b5fd19e4cf6cb640be434972bc7b
UnfazedAy <ayomidesoniyi@gmail.com>
2023-03-07 16:44:12 +0100

    Done with task 5

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -24,7 +24,11 @@ class Auth():
 
     def authorization_header(self, request=None) -> str:
         """A method to authorize the header requests"""
-        return None
+        if request is None:
+            return None
+        if not request.headers.get('Authorization'):
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """current user method"""
