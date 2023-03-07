d4171da4e5431830d51dff07af53a49d81f871a7
UnfazedAy <ayomidesoniyi@gmail.com>
2023-03-07 15:45:51 +0100

    Done wit task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,16 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Public method for require auth"""
-        return False
+
+        unaccepted_path = f"{path}/"
+
+        if path is None:
+            return True
+        elif path in excluded_paths or unaccepted_path in excluded_paths:
+            return False
+        elif excluded_paths is None or excluded_paths == []:
+            return True
+        return True
 
     def authorization_header(self, request=None) -> str:
         """A method to authorize the header requests"""
