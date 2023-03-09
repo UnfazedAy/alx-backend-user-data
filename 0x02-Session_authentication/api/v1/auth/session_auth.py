d20a4b68e8a39b524b1231be4fc8cfc37d57cc47
UnfazedAy <ayomidesoniyi@gmail.com>
2023-03-09 10:30:58 +0100

    Done with task 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env ptyhon3
+"""Session authentication module"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Session authentication class that inherits from Auth"""
+    pass
