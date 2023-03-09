501dc36bcfbf36922559607e28532ec1c2cb0c08
UnfazedAy <ayomidesoniyi@gmail.com>
2023-03-09 19:22:37 +0100

    Done with task 7

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -28,12 +28,12 @@ def auth_session_login() -> Tuple[str, int]:
         return jsonify({"error": "no user found for this email"})
 
     for user in users:
-        if not user.is_valid_password(password):
-            return jsonify({"error": "wrong password"}), 401
-        else:
-          from api.v1.app import auth
-          session_id = auth.create_session(user.id)
-          response = jsonify(user.to_json())
-          session_name = os.getenv('SESSION_NAME')
-          response.set_cookie(session_name, session_id)
-          return response
+        if user.is_valid_password(password):
+            from api.v1.app import auth
+            session_id = auth.create_session(user.id)
+            response = jsonify(user.to_json())
+            session_name = os.getenv('SESSION_NAME')
+            response.set_cookie(session_name, session_id)
+            return response
+
+        return jsonify({"error": "wrong password"}), 401
