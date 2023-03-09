9a44cccc52f5b2544468989755a856619967d14c
UnfazedAy <ayomidesoniyi@gmail.com>
2023-03-09 14:00:29 +0100

    Done with task 5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -29,22 +29,24 @@ elif auth_type == "session_auth":
 @app.before_request
 def before_request() -> None:
     """Executes before the whole routes does"""
-    excluded_path = [
-        '/api/v1/status/',
-        '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
-        '/api/v1/auth_session/login/'
-    ]
 
     if auth is None:
         return
-    if auth.require_auth(request.path, excluded_path):
+
+    excluded_paths = [
+        "/api/v1/status/",
+        "/api/v1/unauthorized/",
+        "/api/v1/forbidden/",
+        "/api/v1/auth_session/login/",
+    ]
+    if auth.require_auth(request.path, excluded_paths):
+        user = auth.current_user(request)
         if auth.authorization_header(request) is None and \
                 auth.session_cookie(request) is None:
             abort(401)
-        if auth.current_user(request) is None:
+        if user is None:
             abort(403)
-        request.current_user = auth.current_user(request)
+        request.current_user = user
 
 
 @app.errorhandler(404)
