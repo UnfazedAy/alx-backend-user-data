ec7244a647d0ce9fa5ab3b9ebb00fe7d7bb9aee1
UnfazedAy <ayomidesoniyi@gmail.com>
2023-03-09 09:03:06 +0100

    Done with task 0

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -27,6 +27,10 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
+    if user_id == "me":
+        if request.current_user is None:
+            abort(404)
+        return jsonify(request.current_user.to_json())
     user = User.get(user_id)
     if user is None:
         abort(404)
@@ -120,3 +124,16 @@ def update_user(user_id: str = None) -> str:
         user.last_name = rj.get('last_name')
     user.save()
     return jsonify(user.to_json()), 200
+
+
+@app_views.route('/users/me', methods=['GET'], strict_slashes=False)
+def get_authenticated_user():
+    """GET /api/v1/users/me
+    Return:
+      - Authenticated User object JSON represented
+      -
+    """
+    if request.current_user:
+        return jsonify(request.current_user.to_json()), 200
+    else:
+        abort(404)
