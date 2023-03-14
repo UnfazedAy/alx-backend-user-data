0f3d35669a23bd1d43baf1a5689258ec3bf533c1
UnfazedAy <ayomidesoniyi@gmail.com>
2023-03-14 21:08:49 +0100

    Done with task 13

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -73,7 +73,7 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """Destroys user session"""
         try:
-            self._db.update_user(id=user_id, session_id=None)
+            self._db.update_user(user_id, session_id=None)
             return None
-        except ValueError:
+        except Exception:
             return None
