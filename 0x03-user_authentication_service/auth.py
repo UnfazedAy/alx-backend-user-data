17be758b9726b8a528a5a90f59873c4038538489
UnfazedAy <ayomidesoniyi@gmail.com>
2023-03-14 16:25:37 +0100

    Fixed task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -28,4 +28,4 @@ class Auth:
                 raise ValueError(f"User {email} already exists")
         except NoResultFound:
             passwd = _hash_password(password).decode('utf-8')
-            return self._db.add_user(email, passwd)
+            return self._db.add_user(email=email, password=passwd)
