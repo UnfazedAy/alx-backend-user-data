679a9cc50ed32c3b1304339a4bad358c89a82a54
UnfazedAy <ayomidesoniyi@gmail.com>
2023-03-14 15:44:28 +0100

    Fixed the echo=True to False

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -19,7 +19,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -33,7 +33,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> None:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """A method to save the user to database"""
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
