d8c094bc72fb7eb43650bcb1cb3205a85dee5d6a
UnfazedAy <ayomidesoniyi@gmail.com>
2023-03-14 20:10:58 +0100

    Done with task 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -40,6 +40,7 @@ def login():
         session_id = AUTH.create_session(email)
         response = jsonify({"email": f"{email}", "message": "logged in"})
         response.set_cookie("session_id", session_id)
+        return response
 
     abort(401)
 
