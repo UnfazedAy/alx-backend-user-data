7c82780abd14ba6782536713daa9ddd37c37a61d
UnfazedAy <ayomidesoniyi@gmail.com>
2023-03-14 16:28:42 +0100

    Fixed task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -20,7 +20,7 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def register_user(self, email: str, password: str) -> User():
+    def register_user(self, email: str, password: str) -> User:
         """Adds a new user to the database"""
         try:
             user = self._db.find_user_by(email=email)
